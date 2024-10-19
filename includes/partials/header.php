50b1ddadc7f2b25369aafdad8526810a2b0fe309
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-10-19 18:26:27 +0000

    feat: add font and bootstrap instead cdn

--- includes/partials/header.php
+++ includes/partials/header.php
@@ -13,7 +13,8 @@ function Header(string $name)
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $name ?> - PosPro</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="/includes/css/bootstrap.css" rel="stylesheet" />
+    <link href="/includes/css/style.css" rel="stylesheet">
   </head>
 
   <body>
