25f0a953c6c791ae6ca11f95ea147ed88e81ace0
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-10-21 18:00:24 +0000

    feat: display user's profile image

--- dashboard.php
+++ dashboard.php
@@ -7,6 +7,7 @@ require_once "./usecases/auth.php";
 require_once "./libs/mysql.php";
 require_once "./libs/constants.php";
 
+use Domain\User;
 use Repository\UserRepository;
 use Usecases\UserUsecases;
 use Libs\MySQL;
@@ -38,22 +39,26 @@ Header("แดชบอร์ด");
 Navbar($user);
 ?>
 <div class="container my-4">
-  <h1 class="text-center">
-    ยินดีต้อนรับคุณ <?= !empty($user->firstname) ? $user->firstname : $user->username ?> !
-  </h1>
-  <p class="text-center">
-    แดชบอร์ด
-  </p>
+  <div class="d-flex flex-column justify-content-center">
+    <?php
+    if ($user->profilePath) {
+    ?>
+      <img src="<?= User::getUserProfilePath($user->profilePath) ?>" width="150" height="150" alt="profile image" class="rounded-circle mx-auto" />
+    <?php
+    }
+    ?>
+    <div class="d-flex flex-column my-auto mt-2">
+      <h1 class="text-center">
+        ยินดีต้อนรับคุณ <?= !empty($user->firstname) ? $user->firstname : $user->username ?> !
+      </h1>
+    </div>
+  </div>
 
-  <div>
+
+  <div class="d-flex flex-column mx-auto text-center mt-5">
     <a href="products.php">จัดการสินค้า</a>
-    <br>
-
     <a href="product-types.php">จัดการประเภทสินค้า</a>
-    <br>
-
     <a href="order.php">จัดการออเดอร์</a>
-    <br>
   </div>
 
 </div>
