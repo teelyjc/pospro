5e19b1d22dfd8b76389438fbb8bc8c29c0ccb359
Tannatee Juchan <ju.tannatee2543@gmail.com>
2024-10-21 10:03:23 +0000

    chore: add some mock up data

---
+++ others/product_types.sql
@@ -0,0 +1,118 @@
+INSERT INTO
+  `product_types` (
+    `id`,
+    `name`,
+    `description`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'a8e8aaf0-7b94-47a3-92b4-4d4b547e838a',
+    'เครื่องดื่มร้อน',
+    'เมนูเครื่องดื่มร้อน เช่น กาแฟ ชา',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `product_types` (
+    `id`,
+    `name`,
+    `description`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'b7c3f1f3-68a0-4de3-854d-07f5d8e19a3d',
+    'เครื่องดื่มเย็น',
+    'เมนูเครื่องดื่มเย็น เช่น น้ำผลไม้ กาแฟเย็น',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `product_types` (
+    `id`,
+    `name`,
+    `description`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'c2a34d45-1263-4f77-95b1-97c0f50ef272',
+    'ขนมอบ',
+    'เมนูขนมอบ เช่น ครัวซองต์ เค้ก',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `product_types` (
+    `id`,
+    `name`,
+    `description`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'd3e5c9f6-9583-49d8-89b4-6e3db69d305e',
+    'อาหารเช้า',
+    'เมนูอาหารเช้า เช่น แซนวิช โจ๊ก',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `product_types` (
+    `id`,
+    `name`,
+    `description`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'e4b6ea87-7b2c-4e1b-bab9-4f8f569e63e7',
+    'อาหารจานหลัก',
+    'เมนูอาหารจานหลัก เช่น ข้าวผัด พาสต้า',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `product_types` (
+    `id`,
+    `name`,
+    `description`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'd7f5c3d5-e3d6-4111-8bbf-fdd2e52761f3',
+    'ผลิตภัณฑ์เสริมอาหาร',
+    'อาหารเสริมสุขภาพเพื่อการบำรุงร่างกาย',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `product_types` (
+    `id`,
+    `name`,
+    `description`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'f8b36d99-2e3c-4a7d-b7a5-e2a1b1ef6f55',
+    'เครื่องครัว',
+    'อุปกรณ์เครื่องครัวเพื่อการทำอาหาร',
+    NOW (),
+    NOW ()
+  );
---
+++ others/products.sql
@@ -0,0 +1,1080 @@
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'f5ca9d9d-f8a9-4dc2-bdcd-2f776ff2b241',
+    'a8e8aaf0-7b94-47a3-92b4-4d4b547e838a',
+    'กาแฟดำร้อน',
+    'กาแฟดำสูตรพิเศษ',
+    '45.00',
+    '100',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'a4c9392b-1122-40b0-9bff-2313b0a3e6ba',
+    'a8e8aaf0-7b94-47a3-92b4-4d4b547e838a',
+    'ชาเขียวร้อน',
+    'ชาเขียวญี่ปุ่นคุณภาพสูง',
+    '50.00',
+    '80',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'c3e9eb56-dac1-4d14-8f2b-3a7d2ab2c8db',
+    'b7c3f1f3-68a0-4de3-854d-07f5d8e19a3d',
+    'น้ำผลไม้ส้มเย็น',
+    'น้ำส้มคั้นสด',
+    '60.00',
+    '60',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'd7f3ed49-58fa-4f47-80de-11363c3021da',
+    'b7c3f1f3-68a0-4de3-854d-07f5d8e19a3d',
+    'กาแฟเย็น',
+    'กาแฟเย็นสูตรพิเศษ',
+    '65.00',
+    '70',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'e8b7abf9-30cf-47a9-8f7e-9f9dbd9a3b25',
+    'c2a34d45-1263-4f77-95b1-97c0f50ef272',
+    'ครัวซองต์',
+    'ครัวซองต์อบกรอบใหม่สดทุกวัน',
+    '75.00',
+    '40',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'f6b7cd23-cd36-4f6f-80fd-4c3bb5ae7ea6',
+    'c2a34d45-1263-4f77-95b1-97c0f50ef272',
+    'เค้กช็อกโกแลต',
+    'เค้กช็อกโกแลตเข้มข้น',
+    '85.00',
+    '30',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'a5b4c761-42db-4e72-8269-1186a1c6f435',
+    'd3e5c9f6-9583-49d8-89b4-6e3db69d305e',
+    'แซนวิชไข่',
+    'แซนวิชไข่พร้อมผักสด',
+    '55.00',
+    '50',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'b6b5dd57-53c1-4ae2-b1f3-e04de1d30e5b',
+    'd3e5c9f6-9583-49d8-89b4-6e3db69d305e',
+    'โจ๊กหมู',
+    'โจ๊กหมูพร้อมเครื่องเคียง',
+    '65.00',
+    '40',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'c6e9e8d1-1096-4500-957f-70b8df869a92',
+    'e4b6ea87-7b2c-4e1b-bab9-4f8f569e63e7',
+    'ข้าวผัดกระเพราไก่',
+    'ข้าวผัดกระเพราไก่รสจัดจ้าน',
+    '75.00',
+    '100',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'd7fa27fa-7525-4f8e-8248-12bf1b6063f3',
+    'e4b6ea87-7b2c-4e1b-bab9-4f8f569e63e7',
+    'พาสต้าคาโบนารา',
+    'พาสต้าคาโบนาราสูตรพิเศษ',
+    '90.00',
+    '50',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'f6c3219b-2739-4a4a-8104-53b7bb5b9f82',
+    'a8e8aaf0-7b94-47a3-92b4-4d4b547e838a',
+    'ลาเต้ร้อน',
+    'ลาเต้ร้อนหอมมัน',
+    '55.00',
+    '90',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'e72b2dc7-23d8-42b2-a819-e9d67fc67054',
+    'a8e8aaf0-7b94-47a3-92b4-4d4b547e838a',
+    'คาปูชิโน่ร้อน',
+    'คาปูชิโน่ร้อนเข้มข้น',
+    '60.00',
+    '80',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'a6d30b8b-1b2e-46a6-a18a-d3b4c573255e',
+    'b7c3f1f3-68a0-4de3-854d-07f5d8e19a3d',
+    'ชาไทยเย็น',
+    'ชาไทยเย็นสูตรเข้มข้น',
+    '50.00',
+    '75',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'b7842b6e-4e42-4c0d-81b0-2d3ef8ff7d2b',
+    'b7c3f1f3-68a0-4de3-854d-07f5d8e19a3d',
+    'โกโก้เย็น',
+    'โกโก้เย็นเข้มข้นหอมหวาน',
+    '65.00',
+    '65',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'c8dbe445-c518-4993-91df-89ab8fbe1733',
+    'b7c3f1f3-68a0-4de3-854d-07f5d8e19a3d',
+    'น้ำอัญชันมะนาว',
+    'น้ำอัญชันสดชื่นพร้อมมะนาว',
+    '45.00',
+    '80',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'db2e5b43-1849-4180-844f-e4a1b4bfea6f',
+    'c2a34d45-1263-4f77-95b1-97c0f50ef272',
+    'โดนัท',
+    'โดนัทเนื้อนุ่มหวานฉ่ำ',
+    '40.00',
+    '60',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'c3a8c850-755d-4f96-a2b9-c4b9dbd6bf2e',
+    'c2a34d45-1263-4f77-95b1-97c0f50ef272',
+    'บลูเบอร์รี่ชีสเค้ก',
+    'ชีสเค้กพร้อมบลูเบอร์รี่',
+    '95.00',
+    '40',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'a5e72323-5b7f-4396-89b8-736b1d5163d8',
+    'd3e5c9f6-9583-49d8-89b4-6e3db69d305e',
+    'ไข่กระทะ',
+    'ไข่กระทะพร้อมไส้กรอกและหมูสับ',
+    '70.00',
+    '50',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'b1b5fbcb-2f0e-44c6-85a9-5d5b482e97b8',
+    'd3e5c9f6-9583-49d8-89b4-6e3db69d305e',
+    'แพนเค้ก',
+    'แพนเค้กเสิร์ฟพร้อมน้ำผึ้งและผลไม้สด',
+    '85.00',
+    '40',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'da8c2c1b-17b8-4d96-b1ff-7980ecfe9f44',
+    'e4b6ea87-7b2c-4e1b-bab9-4f8f569e63e7',
+    'ผัดไทยกุ้งสด',
+    'ผัดไทยสูตรพิเศษใส่กุ้งสด',
+    '100.00',
+    '60',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'f0a2c7ae-5c44-426f-b5f5-e8194a8e1fd6',
+    'e4b6ea87-7b2c-4e1b-bab9-4f8f569e63e7',
+    'สปาเก็ตตี้ซอสมะเขือเทศ',
+    'สปาเก็ตตี้ซอสมะเขือเทศสดใหม่',
+    '95.00',
+    '50',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'b2cf1f07-cb53-45ae-b85b-509cde22996e',
+    'e4b6ea87-7b2c-4e1b-bab9-4f8f569e63e7',
+    'แกงเขียวหวานไก่',
+    'แกงเขียวหวานไก่รสกลมกล่อม',
+    '105.00',
+    '40',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'cb1f2a6b-f2b5-45e4-b7e7-c0d34fd3c04c',
+    'e4b6ea87-7b2c-4e1b-bab9-4f8f569e63e7',
+    'ข้าวผัดอเมริกัน',
+    'ข้าวผัดพร้อมไส้กรอก ไข่ดาว และไก่ทอด',
+    '85.00',
+    '50',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'df2c2a5d-453f-411f-9647-c5d5cb481f0f',
+    'd7f5c3d5-e3d6-4111-8bbf-fdd2e52761f3',
+    'วิตามินซี 1000 มก.',
+    'เสริมสร้างภูมิคุ้มกันและป้องกันหวัด',
+    '350.00',
+    '100',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'da4b1224-e6be-4af9-bcae-1f8e1e07e9b9',
+    'd7f5c3d5-e3d6-4111-8bbf-fdd2e52761f3',
+    'โอเมก้า 3 น้ำมันปลา',
+    'บำรุงสมองและหัวใจจากน้ำมันปลา',
+    '450.00',
+    '80',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'e17c9b98-2df8-4f40-a0e3-9fba9d1dbd56',
+    'd7f5c3d5-e3d6-4111-8bbf-fdd2e52761f3',
+    'แคลเซียมเสริมกระดูก',
+    'เสริมแคลเซียมให้กระดูกและฟันแข็งแรง',
+    '500.00',
+    '75',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'd32e0568-50a2-466e-b545-2c4e65f0b2e2',
+    'd7f5c3d5-e3d6-4111-8bbf-fdd2e52761f3',
+    'วิตามินบีรวม',
+    'ช่วยในการทำงานของระบบประสาทและสมอง',
+    '400.00',
+    '90',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'b45af5c3-564e-49df-b4a5-ef9e6ec1b232',
+    'd7f5c3d5-e3d6-4111-8bbf-fdd2e52761f3',
+    'ซิงค์ วิตามินเสริมภูมิคุ้มกัน',
+    'ช่วยป้องกันและเสริมสร้างภูมิคุ้มกัน',
+    '380.00',
+    '120',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'c82e7611-8f7e-4b11-95b4-e99c14fe541e',
+    'd7f5c3d5-e3d6-4111-8bbf-fdd2e52761f3',
+    'ไฟเบอร์เสริมระบบย่อยอาหาร',
+    'ช่วยในระบบย่อยอาหารและลำไส้ให้ทำงานดีขึ้น',
+    '420.00',
+    '65',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'de637c3a-dbd9-4be1-967d-3b517f1f1241',
+    'd7f5c3d5-e3d6-4111-8bbf-fdd2e52761f3',
+    'แมกนีเซียมเสริม',
+    'บำรุงกล้ามเนื้อและระบบประสาท',
+    '390.00',
+    '100',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'f13e9189-497e-4e39-b9a2-516df629540a',
+    'd7f5c3d5-e3d6-4111-8bbf-fdd2e52761f3',
+    'คอลลาเจนไตรเปปไทด์',
+    'เสริมคอลลาเจนให้ผิวพรรณเปล่งปลั่ง',
+    '600.00',
+    '85',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'c9f6d372-0031-4670-8323-99e8f50c1bb6',
+    'd7f5c3d5-e3d6-4111-8bbf-fdd2e52761f3',
+    'โพรไบโอติกส์',
+    'ช่วยปรับสมดุลระบบย่อยอาหารและลำไส้',
+    '550.00',
+    '70',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'aa146e4a-b20e-4345-bf7d-963b3e1ae43c',
+    'd7f5c3d5-e3d6-4111-8bbf-fdd2e52761f3',
+    'วิตามินดีเสริมกระดูก',
+    'บำรุงกระดูกและฟันให้แข็งแรง',
+    '300.00',
+    '95',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'b0d6a9d3-12eb-4a17-b5a0-7d13bdbd7095',
+    'd7f5c3d5-e3d6-4111-8bbf-fdd2e52761f3',
+    'กรดโฟลิกเสริมสร้างเม็ดเลือด',
+    'บำรุงร่างกายและสร้างเม็ดเลือดแดง',
+    '340.00',
+    '110',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'ebf0b546-bcd6-4a53-9318-c29b6c2528fe',
+    'd7f5c3d5-e3d6-4111-8bbf-fdd2e52761f3',
+    'วิตามินอีบำรุงผิว',
+    'ช่วยบำรุงผิวพรรณและลดรอยเหี่ยวย่น',
+    '480.00',
+    '90',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'fc55a8db-9a8c-46c3-8f46-0f9c947ff3fc',
+    'd7f5c3d5-e3d6-4111-8bbf-fdd2e52761f3',
+    'สารสกัดจากชาเขียว',
+    'เสริมการเผาผลาญและลดน้ำหนัก',
+    '450.00',
+    '60',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'a0c2aafd-42a4-41c7-9994-183154742f19',
+    'f8b36d99-2e3c-4a7d-b7a5-e2a1b1ef6f55',
+    'มีดเชฟ',
+    'มีดเชฟสแตนเลสคมทนทาน',
+    '500.00',
+    '50',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'b0f2ae71-b7af-4c7d-9a6c-1a582dbed3e9',
+    'f8b36d99-2e3c-4a7d-b7a5-e2a1b1ef6f55',
+    'กระทะเทฟลอน',
+    'กระทะเคลือบเทฟลอนกันติด',
+    '650.00',
+    '40',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'c2ae341f-61cf-4f2f-8157-e1934e6c4b9e',
+    'f8b36d99-2e3c-4a7d-b7a5-e2a1b1ef6f55',
+    'เครื่องปั่นอาหาร',
+    'เครื่องปั่นอาหารไฟฟ้า 2 ลิตร',
+    '1200.00',
+    '30',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'd9fd07ba-162d-4a64-8025-684f8f342fa5',
+    'f8b36d99-2e3c-4a7d-b7a5-e2a1b1ef6f55',
+    'ตะหลิวไม้',
+    'ตะหลิวไม้เนื้อแข็งไม่ติดกระทะ',
+    '150.00',
+    '100',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'e2714c32-781d-4a6d-bd6e-31f8a07f38b4',
+    'f8b36d99-2e3c-4a7d-b7a5-e2a1b1ef6f55',
+    'เขียงไม้',
+    'เขียงไม้ขนาดใหญ่สำหรับการหั่น',
+    '350.00',
+    '60',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'a0f3723b-4923-4f76-9d1f-39eb27402955',
+    'f8b36d99-2e3c-4a7d-b7a5-e2a1b1ef6f55',
+    'เครื่องต้มกาแฟ',
+    'เครื่องต้มกาแฟระบบไฟฟ้า',
+    '900.00',
+    '35',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'ba2d0426-4b5f-4a1a-8ae4-5ac6e8d82e9f',
+    'f8b36d99-2e3c-4a7d-b7a5-e2a1b1ef6f55',
+    'เครื่องทอดไร้น้ำมัน',
+    'หม้อทอดไร้น้ำมันขนาด 3 ลิตร',
+    '2800.00',
+    '25',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'c738c31e-4d23-4de5-9c16-f84362e70d8f',
+    'f8b36d99-2e3c-4a7d-b7a5-e2a1b1ef6f55',
+    'ชามผสมสแตนเลส',
+    'ชามผสมอาหารสแตนเลสขนาด 2 ลิตร',
+    '300.00',
+    '100',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'de12f9f4-536e-487a-8cb1-e4bcd9ff7dd0',
+    'f8b36d99-2e3c-4a7d-b7a5-e2a1b1ef6f55',
+    'เครื่องหั่นผัก',
+    'เครื่องหั่นผักมือหมุนใช้ง่าย',
+    '700.00',
+    '45',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'fd96fbb9-7bfa-4d6f-825e-7b8f55c3f53f',
+    'f8b36d99-2e3c-4a7d-b7a5-e2a1b1ef6f55',
+    'ถ้วยตวง',
+    'ถ้วยตวงพลาสติกใสสำหรับการอบขนม',
+    '100.00',
+    '120',
+    NOW (),
+    NOW ()
+  );
+
+INSERT INTO
+  `products` (
+    `id`,
+    `type_id`,
+    `name`,
+    `description`,
+    `price`,
+    `quantity`,
+    `created_at`,
+    `updated_at`
+  )
+VALUES
+  (
+    'c5ffbdb3-82cf-4966-9722-960ff8187a2f',
+    'f8b36d99-2e3c-4a7d-b7a5-e2a1b1ef6f55',
+    'ที่คั้นน้ำส้ม',
+    'ที่คั้นน้ำส้มมือบีบ',
+    '220.00',
+    '80',
+    NOW (),
+    NOW ()
+  );
